b53ac6e001574908b86466db13921c0f90526e49
Phuoc Sang Dang <91709237+PhuocSang16@users.noreply.github.com>
2024-03-25 16:23:21 +0700

    Update train_vnet.py

--- trainer/train_vnet.py
+++ trainer/train_vnet.py
@@ -9,7 +9,7 @@ import random
 from utils.metrics import AverageMeter
 
 # Định nghĩa hàm huấn luyện cho VNet
-def train_vnet(
+def train(
         epoch: int,
         loader: torch.utils.data.DataLoader,
         optimizer: torch.optim.Optimizer,
@@ -51,4 +51,45 @@ def train_vnet(
             losses.update(loss.item(), batch_img.size(0))
             pbar.set_description(f"Epoch {epoch} - Trainig Loss: {losses.avg:.4f}")
 
+    return losses
+
+def validate(mode: str,
+             epoch: int,
+             loader: torch.utils.data.DataLoader,
+             model: torch.nn.Module,
+             device: torch.device,
+             criterion: Dict,
+             weights: torch.tensor,
+             prossesID: int = None
+             ) -> Tuple[int, list]:
+
+    model.eval()
+
+    if mode == 'validation':
+        prefix = 'Validating'
+    elif mode == 'test':
+        prefix = 'Testing'
+    if prossesID is not None:
+        prefix = "[{}]{}".format(prossesID, prefix)
+
+    losses = AverageMeter()
+
+    with torch.inference_mode():
+        with tqdm(total=len(loader), ascii=True, desc=('{}: {:02d}'.format(prefix, epoch))) as pbar:
+            for batch_i, sample in enumerate(loader, 0):
+
+                # ================= Extract Data ==================
+                batch_img = sample['image'].to(device)
+                batch_gt = sample['gt'].to(device)
+
+                # =================== forward =====================
+                main_output, _ = model(batch_img)
+                output_soft = torch.softmax(main_output, dim=1)
+                loss = criterion['ce_loss'](output_soft, batch_gt.unsqueeze(1).long())
+
+                # =================== backward ====================
+                pbar.update()
+
+                losses.update(loss.item(), batch_img.size(0))
+                pbar.set_description(f"Epoch {epoch} - Val Loss: {losses.avg:.4f}")
     return losses
